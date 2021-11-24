d056b9c82a02a61ddbd053520c2430cfdc9f769d
niyrme <niyrme@niyrme.xyz>
2021-11-24 16:49:24 +0100

    update 2021 template

--- 2021/00/main.py
+++ 2021/00/main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def _part1(path: str):
 	with open(path, "r") as f:
 		...
@@ -12,5 +15,24 @@ def solve(path: str, part: int):
 	{1: _part1, 2: _part2}[part](path)
 
 
+def main() -> int:
+	testsFailed = False
+
+	for test in (
+		(1, 1, "./testInputPart1.txt", None),
+		(2, 1, "./testInputPart2.txt", None),
+	):
+		res = solve(test[2])
+		if res != test[3]:
+			print(f"Part {test[0]} | Test {test[1]} failed | Expected {test[3]}, got {res}", file=sys.stderr)
+			testsFailed = True
+
+	if testsFailed is True:
+		return 1
+	else:
+		print(solve("./input.txt"))
+		return 0
+
+
 if __name__ == "__main__":
-	solve("./input.txt")
+	raise SystemExit(main())
