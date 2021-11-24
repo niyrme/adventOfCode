ff68c5b7ece26fa6608cb7870cab68b4ac753a48
niyrme <niyrme@niyrme.xyz>
2021-11-25 00:53:45 +0100

    update 2020 template

--- 2020/00/main.go
+++ 2020/00/main.go
@@ -7,7 +7,11 @@ import (
 	"strings"
 )
 
-func solve(path string) (ret interface{}) {
+func part1(input interface{}) interface{} {}
+
+func part2(input interface{}) interface{} {}
+
+func solve(part int, path string) interface{} {
 	d, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatalf("Error opening input.txt!\n%s", err.Error())
@@ -15,27 +19,31 @@ func solve(path string) (ret interface{}) {
 
 	var lines []string = strings.Split(strings.TrimSpace(string(d)), "\n")
 
-	return
+	return map[int]func(interface{}) interface{}{
+		1: part1,
+		2: part2,
+	}[part](lines)
 }
 
 type test struct {
 	part     int
 	id       int
-	path     string
+	input    []interface{}
 	expected interface{}
+	f        func(interface{}) interface{}
 }
 
 func _main() int {
 	var tests []test = []test{
-		{1, 1, "./testInputPart1.txt", nil},
-		{2, 1, "./testInputPart2.txt", nil},
+		{1, 1, []interface{}{}, nil, part1},
+		{2, 1, []interface{}{}, nil, part2},
 	}
 
 	testsFailed := false
 
 	for _, test := range tests {
-		if ret := solve(test.path); ret != test.expected {
-			log.Println("Part %v | Test %v failed | Expected %v, got %v", test.part, test.id, test.expected, ret)
+		if ret := test.f(test.input); ret != test.expected {
+			log.Println("Part %d | Test %d failed | Expected %v, got %v", test.part, test.id, test.expected, ret)
 			testsFailed = true
 		}
 	}
