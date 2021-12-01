a3a394eba99b05a28d4fd4ddc2f79a2b8a522719
niyrme <niyrme@niyrme.xyz>
2021-12-02 00:56:33 +0100

    update 2021/00

--- 2021/00/main.py
+++ 2021/00/main.py
@@ -2,6 +2,7 @@
 
 import os
 from typing import Callable
+from typing import Literal
 from typing import Sequence
 from typing import Type
 from typing import TypeVar
@@ -19,36 +20,27 @@ def _part2(inp: Sequence[T]) -> T:
 	...
 
 
-def solve(inp: Sequence[T], part: int, typ: Type):
-	return (_part1, _part2)[part - 1]((typ(line) for line in inp))
+def solve(inp: Sequence[T], part: Literal[1, 2], typ: Type):
+	return (_part1, _part2)[part - 1](tuple(typ(line) for line in inp))
 
 
 def main() -> int:
-	ret = pytest.main([__file__, "--no-header", "--no-summary"])
+	ret = pytest.main([__file__, "--no-header"])
 	if ret != pytest.ExitCode.OK:
 		return ret
 	inputPath = os.path.join(os.path.dirname(__file__), "input.txt")
 	with open(inputPath) as inpF:
 		inp = inpF.read().strip().splitlines()
-		print(f"Part 1: {solve(inp, 1, T)}")
-		print(f"Part 2: {solve(inp, 2, T)}")
+		inpType = T
+		print(f"Part 1: {solve(inp, 1, inpType)}")
+		print(f"Part 2: {solve(inp, 2, inpType)}")
 	return 0
 
 
 @pytest.mark.parametrize(
 	("inp", "expected", "func"), (
-		pytest.param(
-			(),
-			0,
-			_part1,
-			id="1 | 1"
-		),
-		pytest.param(
-			(),
-			0,
-			_part2,
-			id="2 | 1"
-		),
+		pytest.param((), 0, _part1, id="1 | 1"),
+		pytest.param((), 0, _part2, id="2 | 1"),
 	),
 )
 def test(inp: Sequence, expected: T, func: Callable[[Sequence[T]], T]):
