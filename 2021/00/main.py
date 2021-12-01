54fab29948db565ca3ebb4e0181d5bfcd1350d5e
niyrme <niyrme@niyrme.xyz>
2021-12-01 06:34:26 +0100

    clean up 2021/00

--- 2021/00/main.py
+++ 2021/00/main.py
@@ -1,48 +1,54 @@
-import sys
 from typing import Callable
-from typing import NamedTuple
 from typing import Sequence
+from typing import Type
+from typing import TypeVar
+
+import pytest
+
+T = TypeVar("T")
 
 
-def _part1(inp: Sequence):
+def _part1(inp: Sequence[T]) -> T:
 	...
 
 
-def _part2(inp: Sequence):
+def _part2(inp: Sequence[T]) -> T:
 	...
 
 
-def solve(inp: Sequence, part: int):
-	return (_part1, _part2)[part - 1](inp)
-
-
-class Test(NamedTuple):
-	part: int
-	test: int
-	func: Callable
-	inp: Sequence
-	expected: None
+def solve(inp: Sequence[T], part: int, typ: Type):
+	lns: list[typ] = []
+	for line in inp:
+		lns.append(typ(line))
+	return (_part1, _part2)[part - 1](lns)
 
 
 def main() -> int:
-	testsFailed = False
+	with open("./input.txt") as inpF:
+		inp = inpF.read().strip().splitlines()
+		print(solve(inp, 1, int))
+		print(solve(inp, 2, int))
+	return 0
 
-	for test in (
-		Test(1, 1, _part1, tuple(), None),
-		Test(2, 1, _part2, tuple(), None),
-	):
-		res = test.func(test.inp)
-		if res != test.expected:
-			print(f"Part {test.part} | Test {test.test} failed | Expected {test.expected}, got {res}", file=sys.stderr)
-			testsFailed = True
 
-	if testsFailed is True:
-		return 1
-	else:
-		with open("./input.txt") as inp:
-			print(solve(inp.read().strip().splitlines(), 1))
-			print(solve(inp.read().strip().splitlines(), 2))
-		return 0
+@pytest.mark.parametrize(
+	("inp", "expected", "fn"), (
+		pytest.param(
+			(),
+			0,
+			_part1,
+			id="1 | 1"
+		),
+		pytest.param(
+			(),
+			0,
+			_part2,
+			id="2 | 1"
+		),
+	),
+)
+def test(inp: Sequence, expected, fn: Callable[[Sequence[T]], T]):
+	assert fn(inp) == expected
 
 
 if __name__ == "__main__":
