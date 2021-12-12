b6d270d0da4e387e4c232e0d2782d2e440ce3279
niyrme <niyrme@niyrme.xyz>
2021-12-12 02:18:17 +0100

    [2021] update 00
    parsing of input will now happen inside of function

--- 2021/00/main.py
+++ 2021/00/main.py
@@ -1,45 +1,47 @@
 #!/usr/bin/env python3
 
 import os
+from typing import Callable
 from typing import Literal
 from typing import Sequence
-from typing import TypeVar
 
 import pytest
 
-T = TypeVar("T")
+
+# replace with whatever type is needed
+T = int
+parseInput: Callable[[str], Sequence[T]] = lambda inp: tuple(T(line) for line in inp.splitlines())
 
 
-def _part1(inp: Sequence[T]) -> int:
+def part1(inp: str) -> int:
+	lines = parseInput(inp)
 	raise NotImplementedError
 
 
-def _part2(inp: Sequence[T]) -> int:
+def part2(inp: str) -> int:
+	lines = parseInput(inp)
 	raise NotImplementedError
-
-
-def solve(inp: Sequence[str], part: Literal[1, 2]) -> int:
-	return (_part1, _part2)[part - 1](tuple(T(line) for line in inp))
 
 
 def main() -> int:
 	inputPath = os.path.join(os.path.dirname(__file__), "input.txt")
 	with open(inputPath) as inpF:
-		inp = inpF.read().strip().splitlines()
-		print(f"Part 1: {solve(inp, 1)}")
-		print(f"Part 2: {solve(inp, 2)}")
+		inp = inpF.read().strip()
+		print(f"Part 1: {part1(inp)}")
+		print(f"Part 2: {part2(inp)}")
 	return 0
 
 
-EXAMPLE_INPUT = ()
+EXAMPLE_INPUT = """
+""".strip()
 @pytest.mark.parametrize(
 	("inp", "expected", "part"), (
 		pytest.param(EXAMPLE_INPUT, 0, 1, id="1 | 1"),
 		pytest.param(EXAMPLE_INPUT, 0, 2, id="2 | 1"),
 	),
 )
-def test(inp: Sequence[T], expected: T, part: Literal[1, 2]):
-	assert solve(inp, part) == expected
+def test(inp: str, expected: int, part: Literal[1, 2]):
+	assert (part1, part2)[part - 1](inp) == expected
 
 
 if __name__ == "__main__":
